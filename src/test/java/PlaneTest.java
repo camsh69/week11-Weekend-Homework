784037d8652bf6436254c01dd300db0f51add66e
camsh69 <camsh69@gmail.com>
2022-02-11 13:04:09 +0000

    more Plane passing tests

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -18,4 +18,14 @@ public class PlaneTest {
     public void hasPlaneType() {
         assertEquals(PlaneType.BOEING747, plane.getPlaneType());
     }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(467, plane.getPlaneType().getCapacity());
+    }
+
+    @Test
+    public void hasZeroTotalWeightAtStart() {
+        assertEquals(0, plane.getTotalWeight());
+    }
 }
