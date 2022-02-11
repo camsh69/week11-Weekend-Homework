0feb310c1066cd963a06f4e0907fbfb51129f8bc
camsh69 <camsh69@gmail.com>
2022-02-11 13:51:32 +0000

    added Pilot passing tests

--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,2 +1,27 @@
+import airlineEmployee.Pilot;
+import airlineEmployee.RankType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before() {
+        pilot = new Pilot("Skye Silimit", RankType.FIRSTOFFICER, "XY12345T");
+    }
+
+    @Test
+    public void hasLicenceNumber() {
+        assertEquals("XY12345T", pilot.getLicenceNumber());
+    }
+
+    @Test
+    public void canChangeLicenceNumber() {
+        pilot.setLicenceNumber("XY12345Q");
+        assertEquals("XY12345Q", pilot.getLicenceNumber());
+    }
 }
