ed94d45a6240141012827083bba197027ad8d3e3
camsh69 <camsh69@gmail.com>
2022-02-11 12:36:35 +0000

    added PlaneType enum

--- src/main/java/plane/PlaneType.java
+++ src/main/java/plane/PlaneType.java
@@ -1,5 +1,17 @@
 package plane;
 
 public enum PlaneType {
+    BOEING747(467),
+    AIRBUSA380(469),
+    AIRBUS340(340);
 
+    private final int capacity;
+
+    PlaneType(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
 }
