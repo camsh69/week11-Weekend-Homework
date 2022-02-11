9067d3996f5f6612c21d07bd5eb2d7abacbfa5ee
camsh69 <camsh69@gmail.com>
2022-02-11 13:18:36 +0000

    adding first failing test for CabinCreMember

--- src/main/java/airlineEmployee/CabinCrewMember.java
+++ src/main/java/airlineEmployee/CabinCrewMember.java
@@ -1,4 +1,8 @@
 package airlineEmployee;
 
-public class CabinCrewMember {
+public class CabinCrewMember extends AirlineEmployee {
+
+    public CabinCrewMember(int name, RankType rankType) {
+        super(name, rankType);
+    }
 }
--- src/test/java/CabinCrewMemberTest.java
+++ src/test/java/CabinCrewMemberTest.java
@@ -1,2 +1,19 @@
+import airlineEmployee.CabinCrewMember;
+import airlineEmployee.RankType;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CabinCrewMemberTest {
+
+    CabinCrewMember cabinCrewMember;
+
+    @Before
+    public void before() {
+    cabinCrewMember = new CabinCrewMember("Mable Able" , RankType.FLIGHTATTENDANT);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals(RankType.FLIGHTATTENDANT, cabinCrewMember.getRankType())
+    }
 }
