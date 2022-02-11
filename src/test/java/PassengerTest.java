e8861bc86e15558b985739fccf77ae764ccbbbd9
camsh69 <camsh69@gmail.com>
2022-02-11 12:10:27 +0000

    getName failing test for Passenger

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+    public void before() {
+        passenger = new Passenger("Joe Smith", 2);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Joe Smith", passenger.getName());
+    }
+
 }
